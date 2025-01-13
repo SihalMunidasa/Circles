d080a76c0b03c827fb2d63d75de7227f9ab824d2
Sihal Munidasa <sihal.munidasa03@gmail.com>
2025-01-13 17:37:57 +0530

    created a third constructor for circle and added setter and getter for color attribute

--- src/packageUno/Circle.java
+++ src/packageUno/Circle.java
@@ -17,15 +17,22 @@ public class Circle {
         color = "Blue";
     }
 
-    // a second constructor where the user create a circle with radius of his/her choice
+    // a second constructor where the user creates a circle with radius of his/her choice
     // but default color remains blue [wonder what the creator's favorite color is :)]
     public Circle (double radius) {
         this.radius = radius;
         color = "Blue";
     }
 
+    // a third constructor where the uer can create his/her own circle
+    // altering the default attribute values
+    public Circle (double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
     // allowing the user to access the attribute 'radius' using a getter
-    // without straight up altering it using circle.radius
+    // without straight-up altering it using circle.radius
     public double getRadius() {
         return radius;
     }
@@ -33,5 +40,21 @@ public class Circle {
     // a behaviour for a circle to calculate the area of a circle object when called
     public double getArea () {
         return Math.PI*radius*radius;
+    }
+
+    // a behaviour for the circle to return its color when prompted
+    public String getColor() {
+        return color;
+    }
+
+    // behaviours to allow user to change the values using setters
+    public void setRadius(double radius) {
+        this.radius = radius;
+        // this keyword is used as a reference to the current object
+        // hence it resolves naming conflicts
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
--- src/packageUno/TestCircle.java
+++ src/packageUno/TestCircle.java
@@ -14,11 +14,23 @@ public class TestCircle {
         // through the object would also not be possible
 
         // using the getters to print out the following
-        System.out.println("The circle has a radius of " + circle1.getRadius() + " and area of " + circle1.getArea());
+        System.out.println("The circle1 has a radius of " + circle1.getRadius() + " and area of " + circle1.getArea() + " and has the color " + circle1.getColor());
 
         Circle circle2 = new Circle(8);
         // a new circle with radius = 8, wonder what color it is?
 
-        System.out.println("The circle has a radius of " + circle2.getRadius() + " and area of " + circle2.getArea());
+        System.out.println("The circle2 has a radius of " + circle2.getRadius() + " and area of " + circle2.getArea() + " and has the color " + circle2.getColor());
+
+        // Instantiating a new third circle object that is not the default values
+        Circle circle3 = new Circle(10, "Red");
+
+        System.out.println("The circle3 has a radius of " + circle3.getRadius() + " and area of " + circle3.getArea() + " and has the color " + circle3.getColor());
+
+        // Changing values
+        circle1.setColor("Green"); circle1.setRadius(5);
+        circle2.setRadius(15);
+
+        System.out.println("The circle1 has a radius of " + circle1.getRadius() + " and area of " + circle1.getArea() + " and has the color " + circle1.getColor());
+        System.out.println("The circle2 has a radius of " + circle2.getRadius() + " and area of " + circle2.getArea() + " and has the color " + circle2.getColor());
     }
 }
